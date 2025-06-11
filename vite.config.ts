ae1e8c269d87f818c10e3d54fcf8ff33bae39f2b
creates999 <akauntoxuesheng@gmail.com>
2025-06-11 20:09:30 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,8 @@ import { defineConfig, loadEnv } from 'vite';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
-      base: './',
+      // 変更点：'./' からリポジトリ名に変更します。
+      base: '/testhoepagedesu-564889c5691112259142bb7a99650071bef6f66d/',
       define: {
         'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
         'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
